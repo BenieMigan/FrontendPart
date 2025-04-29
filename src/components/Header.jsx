7fe2321622c1203288093b5b609b4462acbf241c
Yv60 <raisathjazahou931@gmail.com>
2025-04-29 08:11:46 +0100

    Ajout de l'entete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import './Header.css';  
 
 const Header = () => {
   return (
     <header className="bg-primary text-white py-2">
       <div className="container d-flex justify-content-between align-items-center">
         <div>
-          <span className="me-4">🕒 Ouvert: 8h - 17h30</span>
-          <span>📞 +229 69 85 85 85</span>
+          <a href="tel:+22969858585" className="text-white me-4 no-underline">🕒 Ouvert: 8h - 17h30</a>
+          <a href="tel:+22969858585" className="text-white no-underline">📞 +229 69 85 85 85</a>
         </div>
         <div>
-        <box-icon type='logo' name='twitter'></box-icon>
-          <strong>Service Client</strong>
+          <a href="mailto:service@exemple.com" className="text-white no-underline">
+            <strong>Service Client</strong>
+          </a>
         </div>
       </div>
     </header>
