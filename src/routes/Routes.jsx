88efd2216ae27376083a4fd24a18e468af13bb3a
Yv60 <raisathjazahou931@gmail.com>
2025-06-14 20:27:51 +0100

    page de creaction

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Connexion from '@/feature/Connexion/index'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Connexion from '@/feature/Connexion/index';
+import Register from '@/feature/Register'
 export default function AppRoutes() {
     return (
         <Router>
             <Routes>
-                <Route path = "connexion" element={<Connexion/>} />
+                <Route path="/connexion" element={<Connexion />} />
+                <Route path="/RegisterForm" element={<Register />} />
             </Routes>
         </Router>
-    )
+    );
 }
